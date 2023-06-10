5af2f1bd2149519be6929f7cf96ba6c39b8f76ec
Gpocas <gpocas01@gmail.com>
2023-06-10 14:51:38 -0300

    add cursor logic

--- script.js
+++ script.js
@@ -4,13 +4,20 @@ const form  = document.querySelector('form')
 const enableFields = () => {
 
     let elements = document.querySelectorAll("[class*='osui-dropdown-search pop-comp-ele vscomp-ele'][disabled]")
+    let adressDestiny = document.querySelector("#b22-b11-b8-DropdownSearch")
+
+    adressDestiny.removeAttribute("disabled");
+    adressDestiny.style.pointerEvents="none";
+    adressDestiny.style.cursor="default";
 
     for(let el of elements) {
-    el.removeAttribute("disabled")
+    el.removeAttribute("disabled");
+    el.style.pointerEvents="none";
+    el.style.cursor="default";
+
     }
 
 }
-
 
 
 form.addEventListener('submit', async(event) => {

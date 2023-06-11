0bf6035cec2a5fc57372aa83bb85e1c33643a7f3
Gpocas <gpocas01@gmail.com>
2023-06-11 09:32:50 -0300

    remove selector obsolete

--- script.js
+++ script.js
@@ -4,12 +4,7 @@ const form  = document.querySelector('form')
 const enableFields = () => {
 
     let elements = document.querySelectorAll("[class*='osui-dropdown-search pop-comp-ele vscomp-ele'][disabled]")
-    let adressDestiny = document.querySelector("#b22-b11-b8-DropdownSearch")
-
-    adressDestiny.removeAttribute("disabled");
-    adressDestiny.style.pointerEvents="none";
-    adressDestiny.style.cursor="default";
-
+    
     for(let el of elements) {
     el.removeAttribute("disabled");
     el.style.pointerEvents="none";

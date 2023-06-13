12b88319d9cdd7832ac582b0b1b6d3e60c50af7f
Gpocas <gpocas01@gmail.com>
2023-06-12 21:38:59 -0300

    version 2

--- script.js
+++ script.js
@@ -1,32 +1,55 @@
-const form  = document.querySelector('form')
-
+const form1 = document.querySelector(".form1");
+const form2 = document.querySelector(".form2");
 
 const enableFields = () => {
+  let elements = document.querySelectorAll(
+    "[class*='osui-dropdown-search pop-comp-ele vscomp-ele'][disabled]"
+  );
 
-    let elements = document.querySelectorAll("[class*='osui-dropdown-search pop-comp-ele vscomp-ele'][disabled]")
-    
-    for(let el of elements) {
+  for (let el of elements) {
     el.removeAttribute("disabled");
-    el.style.pointerEvents="none";
-    el.style.cursor="default";
+    el.style.pointerEvents = "none";
+    el.style.cursor = "default";
+  }
+};
 
-    }
+const copyToClipboard = () => {
+    let nf = document.querySelector('[for=b24-b15-b8-Input_ElectronicInvoiceKey]').parentNode.querySelector('span > input').getAttribute('value')
 
-}
+    // Cria um elemento de texto temporário para copiar os dados
+    const tempElement = document.createElement('textarea');
+    tempElement.value = 'test value copy';
 
+    // Adiciona o elemento temporário à página
+    document.body.appendChild(tempElement);
 
-form.addEventListener('submit', async(event) => {
-    event.preventDefault()
+    // Seleciona e copia o conteúdo do elemento temporário
+    tempElement.focus();
+    tempElement.select();
+    document.execCommand('copy');
 
-    const [tab] = await chrome.tabs.query({active: true, currentWindow: true});
+    // Remove o elemento temporário da página
+    document.body.removeChild(tempElement);
+};
 
-    chrome.scripting.executeScript({
+form1.addEventListener("submit", async (event) => {
+  event.preventDefault();
 
-        target: {tabId: tab.id},
-        function: enableFields
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-    });
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: enableFields,
+  });
+});
 
+form2.addEventListener("submit", async (event) => {
+  event.preventDefault();
 
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-})
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: copyToClipboard,
+  });
+});
